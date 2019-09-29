caa588dd48507af4864d67e1674e60793282dfc1
Ben Guo <bg@stripe.com>
2019-09-29 13:36:33 -0700

    Initial commit

---
+++ using-webhooks/client/ios/App (ObjC)/CheckoutViewController.m
@@ -0,0 +1,27 @@
+//
+//  CheckoutViewController.m
+//  app
+//
+//  Created by Ben Guo on 9/29/19.
+//  Copyright © 2019 stripe-samples. All rights reserved.
+//
+
+#import "CheckoutViewController.h"
+
+/**
+* To run this app, you'll need to first run the sample server locally.
+* Follow the "How to run locally" instructions in the root directory's README.md to get started.
+* Once you've started the server, open http://localhost:4242 in your browser to check that the
+* server is running locally.
+* After verifying the sample server is running locally, build and run the app using the iOS simulator.
+*/
+
+@interface CheckoutViewController ()
+
+@end
+
+@implementation CheckoutViewController
+
+@end
+
+// docs_window_spec {"setup-ui-objc": [[21, 25], [26, 57], [59, 60], ["// ..."], [148, 150]]}
---
+++ using-webhooks/client/ios/App (Swift)/CheckoutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CheckoutViewController.swift
+//  app
+//
+//  Created by Yuki Tokuhiro on 9/25/19.
+//  Copyright © 2019 stripe-samples. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * To run this app, you'll need to first run the sample server locally.
+ * Follow the "How to run locally" instructions in the root directory's README.md to get started.
+ * Once you've started the server, open http://localhost:4242 in your browser to check that the
+ * server is running locally.
+ * After verifying the sample server is running locally, build and run the app using the iOS simulator.
+ */
+
+class CheckoutViewController: UIViewController {
+}
+
+// docs_window_spec {"setup-ui-swift": [[21, 51], [52, 53], ["// ..."], [131, 132]]}
