4d585e83e9924a94ea4c0493c0bb8abfc0c1b0f6
Ben Guo <bg@stripe.com>
2019-09-30 23:42:36 -0700

    Update without-webhooks/client/ios-objc/app/CheckoutViewController.m

--- without-webhooks/client/ios-objc/app/CheckoutViewController.m
+++ without-webhooks/client/ios-objc/app/CheckoutViewController.m
@@ -172,7 +172,6 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
                                 break;
                             }
                             case STPPaymentHandlerActionStatusCanceled: {
-                                NSString *message = handleActionError.localizedDescription ?: @"";
                                 [self displayAlertWithTitle:@"Payment canceled" message:handleActionError.localizedDescription ?: @"" restartDemo:NO];
                                 break;
                             }
